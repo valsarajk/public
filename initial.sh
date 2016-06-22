69c829d179675a9c0b68df71079b8d79a4cab290
Valsaraj Karunakaran <valsa_raj@yahoo.com>
2016-06-21 21:00:14 -0400

    inital

---
+++ initial.sh
@@ -0,0 +1,12 @@
+#!/bin/ksh
+###setting hostname
+printf "\nEnter Hostname:"
+read hostname
+echo $hostname /etc/hostname
+##create user name
+groupadd admin
+useradd -g admin vals
+passwd vals
+###add to sudo
+echo "%admin  ALL=(ALL) ALL" >>/etc/sudoers
+su - vals
